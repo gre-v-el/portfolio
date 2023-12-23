8d9a10145c28253fed53ebec03dfee8bbe166a71
gre-v-el <myszkier.gabriel@gmail.com>
2023-12-23 12:40:58 +0100

    Tweaked the background to work well on mobile, improved the reveal effect on load, fixed a bug with mobile navigation.

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,30 +1,44 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import NavButton from './NavButton';
+import { usePathname, useRouter } from 'next/navigation';
+import NavButtonSet from './NavButtonSet';
 
 const MobileNav: React.FC = () => {
+	let [loading, setLoading] = useState(true);
 	let [vis, setVis] = useState(false);
+	const path = usePathname();
+	
+	function clickNav() {
+		setVis(!vis);
+		setLoading(false);
+
+		console.log("vis: " + vis + ", loading: " + loading);
+	}
+
+	useEffect(() => {
+		setVis(false);
+	}, [path]);
+	
+	const cl = loading ? "" : (vis ? "open" : "closed"); 
 
 	return (
 		<>
-			<nav id="mobile-nav" className={vis ? "open" : ""}>
-				<div id="mobile-nav-button" onClick={() => setVis(!vis)}>
-					<span className="material-symbols-outlined">
-						menu
-					</span>
-				</div>
-				<div className="mobile-nav-buttons">
-					<NavButton href='/' icon='home' text='Home'/>
-					<NavButton href='/projects' icon='code_blocks' text='Projects'/>
-					<NavButton href='/education' icon='school' text='Education'/>
-					<NavButton href='/resume' icon='description' text='Resume'/>
-					<NavButton href='/contact' icon='contact_mail' text='Contact'/>
-				</div>
-			</nav>
-			<div className="mobile-nav-padder"></div>
+		<nav id="mobile-nav" className={cl}>
+			<div id="mobile-nav-button" onClick={clickNav}>
+				<span className="material-symbols-outlined">
+					menu
+				</span>
+			</div>
+			<div className="mobile-nav-buttons">
+				<NavButtonSet/>
+			</div>
+		</nav>
+		<div className="mobile-nav-padder"></div>
 		</>
-	);
-};
-
-export default MobileNav;
+		);
+	};
+	
+	export default MobileNav;
+	
