2058157ebdc6afdf0bb1aeccfe507b15142d809a
gre-v-el <myszkier.gabriel@gmail.com>
2024-01-04 11:58:31 +0100

    Added gallery thumbnail clicking

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import Image from "next/image";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import VideoImage from "./VideoImage";
 
 export default function Gallery({
@@ -11,19 +11,39 @@ export default function Gallery({
 	style: any, 
 	images: string[]
 }) {
+	let thumbsRef = useRef<HTMLDivElement>(null);
 	let [viewed_index, setViewedIndex] = useState(0);
+	
+	function setViewed(index: number) {
+		setViewedIndex(index);
+		
+		if (thumbsRef.current == null) return;
 
+		let width = thumbsRef.current.clientWidth;
+		let left = thumbsRef.current.scrollLeft;
+
+		let target_start = index * 100;
+		let target_end = target_start + 105;
+
+		console.log(target_start, target_end, left, width);
+
+		if (target_start < left) {
+			thumbsRef.current.scrollLeft = target_start;
+		} else if (target_end > left + width) {
+			thumbsRef.current.scrollLeft = target_end - width;
+		}
+	}
 
 	return (
 		<div className={style["gallery"]}>
 			<div className={style["gallery-big-img"]}>
 				<VideoImage src={images[viewed_index]} vid_autostart={true}/>
 			</div>
-			<div className={style["gallery-thumbs"]}>
+			<div className={style["gallery-thumbs"]} ref={thumbsRef}>
 				{images.map((img, index) => (
 					<div 
 						key={index}
-						onClick={() => setViewedIndex(index)}
+						onClick={() => setViewed(index)}
 					>
 						<VideoImage src={img}/>
 					</div>
